e6799b280b7d44cd0e1511c2a12a342c38aea671
dw523 <dan.jk.wright@gmail.com>
2016-04-17 13:36:25 -0400

    Checking in additional test cases, to more thoroughly exercise the calculateMarkup function.

--- spec/nupackSpec.js
+++ spec/nupackSpec.js
@@ -40,5 +40,121 @@ describe('nupack', function () {
       expect(nupack.calculateMarkup(project)).toBe('$13707.63');
       done();
     });
+
+    it('calculates the correct markup when food is used', function (done) {
+      var project = {
+        basePrice: '$1000.00',
+        people: 1,
+        materials: [
+          'food'
+        ]
+      };
+
+      expect(nupack.calculateMarkup(project)).toBe('$1199.10');
+      done();
+    });
+
+    it('calculates the correct markup when pharmaceuticals are used', function (done) {
+      var project = {
+        basePrice: '$1000.00',
+        people: 1,
+        materials: [
+          'pharmaceuticals'
+        ]
+      };
+
+      expect(nupack.calculateMarkup(project)).toBe('$1141.35');
+      done();
+    });
+
+    it('calculates the correct markup when drugs (pharmaceuticals) are used', function (done) {
+      var project = {
+        basePrice: '$1000.00',
+        people: 1,
+        materials: [
+          'drugs'
+        ]
+      };
+
+      expect(nupack.calculateMarkup(project)).toBe('$1141.35');
+      done();
+    });
+
+    it('calculates the correct markup when electronics are used', function (done) {
+      var project = {
+        basePrice: '$1000.00',
+        people: 1,
+        materials: [
+          'electronics'
+        ]
+      };
+
+      expect(nupack.calculateMarkup(project)).toBe('$1083.60');
+      done();
+    });
+
+    it('calculates the correct markup when other materials are used', function (done) {
+      var project = {
+        basePrice: '$1000.00',
+        people: 1,
+        materials: [
+          'books'
+        ]
+      };
+
+      expect(nupack.calculateMarkup(project)).toBe('$1062.60');
+      done();
+    });
+
+    it('calculates the correct markup when multiple materials are used', function (done) {
+      var project = {
+        basePrice: '$1000.00',
+        people: 1,
+        materials: [
+          'books',
+          'food',
+          'electronics',
+          'drugs'
+        ]
+      };
+
+      expect(nupack.calculateMarkup(project)).toBe('$1298.85');
+      done();
+    });
+
+    it('calculates the correct markup when no materials are used', function (done) {
+      var project = {
+        basePrice: '$1000.00',
+        people: 1
+      };
+
+      expect(nupack.calculateMarkup(project)).toBe('$1062.60');
+      done();
+    });
+
+    it('calculates the correct markup when no people are used', function (done) {
+      var project = {
+        basePrice: '$1000.00',
+        materials: [
+          'books'
+        ]
+      };
+
+      expect(nupack.calculateMarkup(project)).toBe('$1050.00');
+      done();
+    });
+
+    it('calculates the correct markup when multiple people are used', function (done) {
+      var project = {
+        basePrice: '$1000.00',
+        people: 4,
+        materials: [
+          'books'
+        ]
+      };
+
+      expect(nupack.calculateMarkup(project)).toBe('$1100.40');
+      done();
+    });
   });
 });
